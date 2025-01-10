ab54fa1969cd49bb721c2228d51318c28daa3461
IKHINtech <sarikhinlovestory@gmail.com>
2025-01-10 11:41:45 +0700

    [update] add animated padding

--- lib/features/SplashScreen.dart
+++ lib/features/SplashScreen.dart
@@ -9,6 +9,9 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double padding1 = 40;
+  double padding2 = 40;
+  double padding3 = 16;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,30 +19,35 @@ class _SplashScreenState extends State<SplashScreen> {
       width: double.infinity,
       height: MediaQuery.of(context).size.height,
       child: Center(
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.2),
-            borderRadius: const BorderRadius.all(Radius.circular(210 / 1)),
-          ),
+        child: AnimatedPadding(
+          padding: EdgeInsets.all(padding1), // Padding pertama
+          duration: const Duration(microseconds: 500), // Durasi animasi
+          curve: Curves.easeInOut, // Jenis kurva animasi
           child: Container(
             padding: const EdgeInsets.all(40),
             decoration: BoxDecoration(
               color: Colors.white.withOpacity(0.2),
-              borderRadius: const BorderRadius.all(Radius.circular(170 / 1)),
+              borderRadius: const BorderRadius.all(Radius.circular(210 / 1)),
             ),
             child: Container(
-              height: 130,
-              width: 130,
-              padding: const EdgeInsets.all(16), // Add padding.
+              padding: const EdgeInsets.all(40),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius:
-                    BorderRadius.circular(130 / 2), // Set border radius.
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: const BorderRadius.all(Radius.circular(170 / 1)),
               ),
-              child: Image.asset(
-                'assets/images/logo_indoquran.png',
-                fit: BoxFit.cover,
+              child: Container(
+                height: 130,
+                width: 130,
+                padding: const EdgeInsets.all(16), // Add padding.
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.circular(130 / 2), // Set border radius.
+                ),
+                child: Image.asset(
+                  'assets/images/logo_indoquran.png',
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
